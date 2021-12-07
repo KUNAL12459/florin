de9ad9fa8bd88c26776a8a95edaaa8c1c9c39c30
heyaaman <heyaaman2004@gmail.com>
2021-12-07 12:44:12 +0530

    Update chatstatus.py

--- FlorinaRobot/modules/helper_funcs/telethn/chatstatus.py
+++ FlorinaRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from SaitamaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from SaitamaRobot import DRAGONS
+from FlorinaRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from FlorinaRobot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
