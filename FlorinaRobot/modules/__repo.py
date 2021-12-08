4fadb5cfa8830e1626024d92a20bef12a172eb3b
KUNAL12459 <94036274+KUNAL12459@users.noreply.github.com>
2021-12-08 16:48:11 +0530

    Update __repo.py

--- FlorinaRobot/modules/__repo.py
+++ FlorinaRobot/modules/__repo.py
@@ -5,7 +5,7 @@ from pyrogram.types import *
 from FlorinaRobot.conf import get_str_key
 from FlorinaRobot import pbot
 
-REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/cab6825dea9263d347831.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [OWNER](t.me/hehe860) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @THN_BOTS_SUPPORT «««"
+REPO_TEXT = "**A Powerful [FlorinaRobot](https://telegra.ph/file/bbaa5ef130c385e8333f8.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [OWNER](t.me/hehe860) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @THN_BOTS_SUPPORT «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
