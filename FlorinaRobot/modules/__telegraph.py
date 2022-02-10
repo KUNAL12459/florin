f400db2fc23705e1f4009caabd12364f5830b3c2
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-10 12:51:16 +0530

    Update __telegraph.py

--- FlorinaRobot/modules/__telegraph.py
+++ FlorinaRobot/modules/__telegraph.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 saurabh = "FLORINAROBOT"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=manjeet)
+r = telegraph.create_account(short_name=saurabh)
 auth_url = r["auth_url"]
 
 
