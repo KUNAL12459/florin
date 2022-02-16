610af4ce1475115b47f8f7f306debcd3ca1e30ad
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-16 17:49:58 +0530

    Update __repo.py

--- FlorinaRobot/modules/__repo.py
+++ FlorinaRobot/modules/__repo.py
@@ -5,18 +5,18 @@ from pyrogram.types import *
 from FlorinaRobot.conf import get_str_key
 from FlorinaRobot import pbot
 
-REPO_TEXT = "**A Powerful [Robot](https://telegra.ph/file/bbaa5ef130c385e8333f8.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [OWNER](t.me/hehe860) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @THN_BOTS_SUPPORT «««"
+REPO_TEXT = "**A Powerful [Robot](https://telegra.ph/file/bbaa5ef130c385e8333f8.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [OWNER](t.me/hehe860) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @ALEXIA_SUPPORT «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
         InlineKeyboardButton("⚡ 𝗦𝗼𝘂𝗿𝗰𝗲🔥", url=f"https://github.com/KUNAL12459/FlorinaRobot"),
-        InlineKeyboardButton(" 𝗝𝗢𝗜𝗡 🤖", url=f"https://t.me/florina_support"),
+        InlineKeyboardButton(" 𝗝𝗢𝗜𝗡 🤖", url=f"https://t.me/ALEXIA_SUPPORT"),
       ],[
-        InlineKeyboardButton("𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥🇮🇳", url="https://t.me/hehe860"),
-        InlineKeyboardButton("𝗦𝗨𝗣𝗣𝗢𝗥𝗧 💬", url="https://t.me/florina_support"),
+        InlineKeyboardButton("𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥🇮🇳", url="https://t.me/Saur12p"),
+        InlineKeyboardButton("𝗦𝗨𝗣𝗣𝗢𝗥𝗧 💬", url="https://t.me/ALEXIA_SUPPORT"),
       ],[
-        InlineKeyboardButton("📡 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 📡", url="https://t.me/florina_channel"),
-        InlineKeyboardButton("𝗗𝗘𝗩𝗟𝗢𝗣𝗘𝗥 ➽", url="https://t.me/hehe860"),
+        InlineKeyboardButton("📡 𝗨𝗣𝗗𝗔𝗧𝗘𝗦 📡", url="https://t.me/ALEXIA_UPDATE"),
+        InlineKeyboardButton("𝗗𝗘𝗩𝗟𝗢𝗣𝗘𝗥 ➽", url="https://t.me/Saur12p"),
       ]]
     )
   
