f3f161dd670b6bfd882ff0cc4a1d723cd910997a
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-17 10:01:45 +0530

    Update afk.py

--- FlorinaRobot/modules/afk.py
+++ FlorinaRobot/modules/afk.py
@@ -58,14 +58,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "[{} is here!](By @ALEXIA_SUPPORT)",
+                "[{} is back!](By @ALEXIA_SUPPORT)",
+                "[{} is now in the chat!](By @ALEXIA_SUPPORT)",
+                "[{} is awake!](By @ALEXIA_SUPPORT)",
+                "[{} is back online!](By @ALEXIA_SUPPORT)",
+                "[{} is finally here!](By @ALEXIA_SUPPORT)",
+                "[{} Welcome back!](By @ALEXIA_SUPPORT)",
+                "[Where is {}?\nIn the chat!](By @ALEXIA_SUPPORT)",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
