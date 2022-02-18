c0ef8c2dacddb0a8a9c1738d1dc83006dc8b7ac5
SAURABH <94036274+KUNAL12459@users.noreply.github.com>
2022-02-18 06:35:33 +0530

    Update mongo.py

--- FlorinaRobot/mongo.py
+++ FlorinaRobot/mongo.py
@@ -2,14 +2,14 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from FlorinaRobot import MONGO_DB_URI 
+from FlorinaRobot import MONGO_DB_URL 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from FlorinaRobot.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
+MONGO_DB_URI = get_str_key("MONGO_DB_URL")
 MONGO_DB = "FlorinaRobot"
 
 
